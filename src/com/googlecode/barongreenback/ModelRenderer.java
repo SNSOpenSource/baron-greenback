6df1d01e90cd74f852b15d31ff55c117c2ad06ff
Daniel Worthington-Bodart <dan@bodar.com>
2011-07-25 17:37:27 +0100

    Got some initial search working

---
+++ src/com/googlecode/barongreenback/ModelRenderer.java
@@ -0,0 +1,23 @@
+package com.googlecode.barongreenback;
+
+import com.googlecode.barongreenback.web.WebApplication;
+import com.googlecode.funclate.Model;
+import com.googlecode.funclate.stringtemplate.EnhancedStringTemplateGroup;
+import com.googlecode.utterlyidle.Renderer;
+import org.antlr.stringtemplate.StringTemplate;
+
+import static com.googlecode.totallylazy.URLs.packageUrl;
+
+public class ModelRenderer implements Renderer<Model> {
+    private final String name;
+
+    public ModelRenderer(String name) {
+        this.name = name;
+    }
+
+    public String render(Model value) throws Exception {
+        EnhancedStringTemplateGroup group = new EnhancedStringTemplateGroup(packageUrl(WebApplication.class));
+        StringTemplate template = group.getInstanceOf(name, value.toMap());
+        return template.toString();
+    }
+}
--- src/com/googlecode/barongreenback/SearchModule.java
+++ src/com/googlecode/barongreenback/SearchModule.java
@@ -1,25 +1,40 @@
 package com.googlecode.barongreenback;
 
+import com.googlecode.funclate.Model;
 import com.googlecode.totallylazy.records.Records;
 import com.googlecode.totallylazy.records.lucene.LuceneRecords;
 import com.googlecode.utterlyidle.Resources;
+import com.googlecode.utterlyidle.handlers.ResponseHandlers;
 import com.googlecode.utterlyidle.modules.ApplicationScopedModule;
 import com.googlecode.utterlyidle.modules.Module;
 import com.googlecode.utterlyidle.modules.RequestScopedModule;
 import com.googlecode.utterlyidle.modules.ResourcesModule;
+import com.googlecode.utterlyidle.modules.ResponseHandlersModule;
 import com.googlecode.yadic.Container;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.util.Version;
 
+import static com.googlecode.totallylazy.Predicates.instanceOf;
+import static com.googlecode.totallylazy.Predicates.is;
+import static com.googlecode.totallylazy.Predicates.where;
+import static com.googlecode.totallylazy.proxy.Call.method;
+import static com.googlecode.totallylazy.proxy.Call.on;
 import static com.googlecode.utterlyidle.annotations.AnnotatedBindings.annotatedClass;
+import static com.googlecode.utterlyidle.dsl.BindingBuilder.definedParam;
+import static com.googlecode.utterlyidle.dsl.BindingBuilder.get;
+import static com.googlecode.utterlyidle.dsl.DslBindings.binding;
+import static com.googlecode.utterlyidle.handlers.HandlerRule.entity;
+import static com.googlecode.utterlyidle.handlers.RenderingResponseHandler.renderer;
 
-public class SearchModule implements ResourcesModule, ApplicationScopedModule, RequestScopedModule {
-    public Module addResources(Resources resources) {
+public class SearchModule implements ResourcesModule, ApplicationScopedModule, RequestScopedModule, ResponseHandlersModule {
+    public Module addResources(Resources resources) throws ParseException {
         resources.add(annotatedClass(SearchResource.class));
+        resources.add(binding(get("").resource(method(on(SearchResource.class).find(definedParam("type:users"))))));
         return this;
     }
 
@@ -35,6 +50,11 @@ public class SearchModule implements ResourcesModule, ApplicationScopedModule, R
         container.addActivator(Records.class, container.getActivator(LuceneRecords.class));
         container.add(Analyzer.class, StandardAnalyzer.class);
         container.addActivator(QueryParser.class, QueryParserActivator.class);
+        return this;
+    }
+
+    public Module addResponseHandlers(ResponseHandlers handlers) {
+        handlers.add(where(entity(), is(instanceOf(Model.class))), renderer(new ModelRenderer("search")));
         return this;
     }
 }
