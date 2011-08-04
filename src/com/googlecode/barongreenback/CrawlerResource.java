9c3c0fb9915eb161428d65c37288050f4c4f4140
Daniel Worthington-Bodart <dan@bodar.com>
2011-08-04 16:01:13 +0100

    WIP: Crawling sub feeds

--- src/com/googlecode/barongreenback/CrawlerResource.java
+++ src/com/googlecode/barongreenback/CrawlerResource.java
@@ -1,12 +1,8 @@
 package com.googlecode.barongreenback;
 
 import com.googlecode.funclate.Model;
-import com.googlecode.totallylazy.Callable1;
-import com.googlecode.totallylazy.Pair;
-import com.googlecode.totallylazy.Quadruple;
-import com.googlecode.totallylazy.Sequence;
-import com.googlecode.totallylazy.Sequences;
-import com.googlecode.totallylazy.Triple;
+import com.googlecode.totallylazy.*;
+import com.googlecode.totallylazy.Callables;
 import com.googlecode.totallylazy.records.ImmutableKeyword;
 import com.googlecode.totallylazy.records.Keyword;
 import com.googlecode.totallylazy.records.Record;
@@ -58,12 +54,52 @@ public class CrawlerResource {
                           @FormParam("types") Iterable<String> types, @FormParam("keys") Iterable<String> keys,
                           FormParameters form
     ) throws Exception {
-        Sequence<Pair<Boolean, Keyword>> pairs = toKeywords(fields, aliases, types, clean(keys));
-        Sequence<Keyword> primaryKeys = pairs.filter(where(first(Boolean.class), is(true))).map(second(Keyword.class));
+        Sequence<Triple<Boolean, Keyword, String>> pairs = toKeywords(fields, aliases, types, clean(keys));
+        Sequence<Keyword> primaryKeys = primaryKeys(pairs);
+        Sequence<Pair<Keyword, String>> subFeeds = pairs.filter(where(third(String.class), is(not(Strings.empty())))).map(asSubFeed());
         Sequence<Keyword> allKeys = pairs.map(second(Keyword.class));
         XmlSource webSource = new XmlSource(url, keyword(elementXPath), allKeys);
         Sequence<Record> extractedValues = crawler.crawl(webSource);
-        return put(keyword(recordName), primaryKeys, extractedValues);
+        Sequence<Record> allData = subFeeds.fold(extractedValues, processSubFeed(form));
+        return put(keyword(recordName), primaryKeys, allData);
+    }
+
+    private Sequence<Keyword> primaryKeys(Sequence<Triple<Boolean, Keyword, String>> pairs) {
+        return pairs.filter(where(first(Boolean.class), is(true))).map(second(Keyword.class));
+    }
+
+    private Callable2<Sequence<Record>, Pair<Keyword, String>, Sequence<Record>> processSubFeed(final FormParameters form) {
+        return new Callable2<Sequence<Record>, Pair<Keyword, String>, Sequence<Record>>() {
+            public Sequence<Record> call(Sequence<Record> recordSequence, Pair<Keyword, String> pair) throws Exception {
+                Sequence<Triple<Boolean, Keyword, String>> tripleSequence = extractKeywordsFrom(pair.second(), form);
+                Sequence<Keyword> keys = tripleSequence.map(Callables.second(Keyword.class));
+                return recordSequence.flatMap(crawler.crawl(pair.first(), keyword("/"), keys.toArray(Keyword.class)));
+            }
+        };
+    }
+
+    private Sequence<Triple<Boolean, Keyword, String>> extractKeywordsFrom(String prefix, FormParameters form) {
+        Iterable<String> fields = form.getValues(prefix + "fields");
+        Iterable<String> aliases = form.getValues(prefix + "aliases");
+        Iterable<String> types = form.getValues(prefix + "types");
+        Iterable<String> keys = form.getValues(prefix + "keys");
+        return toKeywords(fields, aliases, types, clean(keys));
+    }
+
+    private Callable1<? super Triple<Boolean, Keyword, String>, Pair<Keyword, String>> asSubFeed() {
+        return new Callable1<Triple<Boolean, Keyword, String>, Pair<Keyword, String>>() {
+            public Pair<Keyword, String> call(Triple<Boolean, Keyword, String> triple) throws Exception {
+                return Pair.pair(triple.second(), triple.third());
+            }
+        };
+    }
+
+    public static <T> Callable1<? super Third<T>, T> third(Class<T> aClass) {
+        return new Callable1<Third<T>, T>() {
+            public T call(Third<T> third) throws Exception {
+                return third.third();
+            }
+        };
     }
 
     private Iterable<Boolean> clean(Iterable<String> keys) {
@@ -78,17 +114,24 @@ public class CrawlerResource {
         };
     }
 
-    private Sequence<Pair<Boolean, Keyword>> toKeywords(Iterable<String> fields, Iterable<String> aliases, Iterable<String> types, Iterable<Boolean> keys) {
-        return Sequences.zip(fields, aliases, types, keys).filter(where(first(String.class), not(empty()))).map(asKeyword());
+    private Sequence<Triple<Boolean, Keyword, String>> toKeywords(Iterable<String> fields, Iterable<String> aliases, Iterable<String> types, Iterable<Boolean> keys) {
+        return Sequences.zip(fields, aliases, types, keys).filter(where(first(String.class), not(empty()))).map(asKeyword()).realise();
     }
 
-    private Callable1<? super Quadruple<String, String, String, Boolean>, Pair<Boolean, Keyword>> asKeyword() {
-        return new Callable1<Quadruple<String, String, String, Boolean>, Pair<Boolean, Keyword>>() {
-            public Pair<Boolean, Keyword> call(Quadruple<String, String, String, Boolean> quadruple) throws Exception {
-                return Pair.pair(quadruple.fourth(), toKeyword(quadruple));
-
+    private Callable1<? super Quadruple<String, String, String, Boolean>, Triple<Boolean, Keyword, String>> asKeyword() {
+        return new Callable1<Quadruple<String, String, String, Boolean>, Triple<Boolean, Keyword, String>>() {
+            public Triple<Boolean, Keyword, String> call(Quadruple<String, String, String, Boolean> quadruple) throws Exception {
+                return Triple.triple(quadruple.fourth(), toKeyword(quadruple), extractSubFeed(quadruple.third()));
             }
         };
+    }
+
+    private String extractSubFeed(String type) {
+        String[] parts = type.split("#");
+        if(parts.length > 1){
+            return parts[1];
+        }
+        return "";
     }
 
     private Keyword toKeyword(Triple<String, String, String> triple) throws ClassNotFoundException {
