4e9e0ee24373be38ec4bc140a1b0b8ff117cb5cb
Mateusz Kwasniewski <kwasniew@users.noreply.github.com>
2011-09-15 12:04:52 +0100

    edit and delete jobs

--- src/com/googlecode/barongreenback/jobs/HttpScheduler.java
+++ src/com/googlecode/barongreenback/jobs/HttpScheduler.java
@@ -1,37 +1,42 @@
 package com.googlecode.barongreenback.jobs;
 
+import com.googlecode.totallylazy.Option;
 import com.googlecode.totallylazy.Sequence;
 import com.googlecode.totallylazy.records.Keyword;
 import com.googlecode.totallylazy.records.Record;
 import com.googlecode.totallylazy.records.RecordMethods;
 import com.googlecode.totallylazy.records.Records;
-import com.googlecode.totallylazy.records.Using;
 import com.googlecode.totallylazy.records.memory.MemoryRecords;
 import com.googlecode.utterlyidle.Application;
+import com.googlecode.utterlyidle.HttpMessageParser;
 import com.googlecode.utterlyidle.Request;
-import com.googlecode.utterlyidle.Response;
-import com.googlecode.utterlyidle.handlers.ClientHttpHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import static com.googlecode.totallylazy.Predicates.is;
+import static com.googlecode.totallylazy.Predicates.where;
 import static com.googlecode.totallylazy.records.Keywords.keyword;
 import static com.googlecode.totallylazy.records.Using.using;
+import static com.googlecode.utterlyidle.HttpMessageParser.parseRequest;
 
 public class HttpScheduler {
     public static final Keyword<String> SCHEDULED_REQUESTS = keyword("scheduledRequests", String.class);
 
+
     public static final Keyword<String> REQUEST = keyword("request", String.class);
+    public static final Keyword<String> JOB_ID = keyword("scheduledRequests_id", String.class);
 
     public static final Keyword<Long> INITIAL_DELAY = keyword("initialDelay", Long.class);
-    public static final Keyword<Long> DELAY = keyword("delay", Long.class);
+    public static final Keyword<Long> INTERVAL = keyword("delay", Long.class);
     public static final Keyword<TimeUnit> TIME_UNIT = keyword("timeUnit", TimeUnit.class);
 
     private final Records records = new MemoryRecords();
-    private final Map<Request, ScheduledFuture<?>> scheduledFutures = new HashMap<Request, ScheduledFuture<?>>();
+    private final Map<String, ScheduledFuture<?>> scheduledFutures = new HashMap<String, ScheduledFuture<?>>();
 
     private final ScheduledExecutorService executorService;
     private final Application application;
@@ -39,17 +44,48 @@ public class HttpScheduler {
     public HttpScheduler(final ScheduledExecutorService executorService, final Application application) {
         this.executorService = executorService;
         this.application = application;
-        records.define(SCHEDULED_REQUESTS, REQUEST, INITIAL_DELAY, DELAY, TIME_UNIT);
+        records.define(SCHEDULED_REQUESTS, JOB_ID, REQUEST, INITIAL_DELAY, INTERVAL, TIME_UNIT);
     }
 
-    public synchronized void schedule(Request request, Record schedulerSpec) {
-        records.put(SCHEDULED_REQUESTS, RecordMethods.update(using(REQUEST), schedulerSpec.set(REQUEST, request.toString())));
+    public synchronized UUID schedule(Request request, Record schedulerSpec) {
+        UUID id = UUID.randomUUID();
+        records.add(SCHEDULED_REQUESTS, schedulerSpec.set(REQUEST, request.toString()).set(JOB_ID, id.toString()));
 
-        if(scheduledFutures.containsKey(request)) {
-            scheduledFutures.get(request).cancel(true);
+        return schedule(id, request, schedulerSpec.get(INITIAL_DELAY), schedulerSpec.get(INTERVAL), schedulerSpec.get(TIME_UNIT));
+    }
+
+    public synchronized UUID reschedule(UUID id, Record schedulerSpec) {
+        records.put(SCHEDULED_REQUESTS, RecordMethods.update(using(JOB_ID), schedulerSpec.set(JOB_ID, id.toString())));
+
+        cancelScheduledFuture(id);
+
+        Record record = job(id).get();
+        return schedule(id, parseRequest(record.get(REQUEST)), record.get(INITIAL_DELAY), record.get(INTERVAL), record.get(TIME_UNIT));
+    }
+
+    public synchronized void remove(UUID id) {
+        cancelScheduledFuture(id);
+        records.remove(SCHEDULED_REQUESTS, where(JOB_ID, is(id.toString())));
+    }
+
+    public Sequence<Record> jobs() {
+        return records.get(SCHEDULED_REQUESTS);
+    }
+
+    public Option<Record> job(UUID id) {
+        return records.get(SCHEDULED_REQUESTS).filter(where(JOB_ID, is(id.toString()))).headOption();
+    }
+
+    private UUID schedule(UUID id, Request request, Long initialDelay, Long interval, TimeUnit timeUnit) {
+        ScheduledFuture<?> scheduledFuture = executorService.scheduleWithFixedDelay(httpTask(request), initialDelay, interval, timeUnit);
+        scheduledFutures.put(id.toString(), scheduledFuture);
+        return id;
+    }
+
+    private void cancelScheduledFuture(UUID id) {
+        if (scheduledFutures.containsKey(id.toString())) {
+            scheduledFutures.get(id.toString()).cancel(true);
         }
-        ScheduledFuture<?> scheduledFuture = executorService.scheduleWithFixedDelay(httpTask(request), schedulerSpec.get(INITIAL_DELAY), schedulerSpec.get(DELAY), schedulerSpec.get(TIME_UNIT));
-        scheduledFutures.put(request, scheduledFuture);
     }
 
     public Runnable httpTask(final Request request) {
@@ -62,9 +98,5 @@ public class HttpScheduler {
                 }
             }
         };
-    }
-
-    public Sequence<Record> jobs() {
-        return records.get(SCHEDULED_REQUESTS);
     }
 }
--- src/com/googlecode/barongreenback/jobs/JobsResource.java
+++ src/com/googlecode/barongreenback/jobs/JobsResource.java
@@ -3,36 +3,28 @@ package com.googlecode.barongreenback.jobs;
 import com.googlecode.funclate.Model;
 import com.googlecode.totallylazy.Callable1;
 import com.googlecode.totallylazy.Sequence;
-import com.googlecode.totallylazy.Uri;
-import com.googlecode.totallylazy.records.MapRecord;
 import com.googlecode.totallylazy.records.Record;
-import com.googlecode.utterlyidle.BasePath;
 import com.googlecode.utterlyidle.HttpMessageParser;
 import com.googlecode.utterlyidle.Request;
-import com.googlecode.utterlyidle.RequestBuilder;
 import com.googlecode.utterlyidle.Response;
-import com.googlecode.utterlyidle.Responses;
-import com.googlecode.utterlyidle.Status;
-import com.googlecode.utterlyidle.annotations.DELETE;
+import com.googlecode.utterlyidle.annotations.FormParam;
 import com.googlecode.utterlyidle.annotations.GET;
 import com.googlecode.utterlyidle.annotations.POST;
-import com.googlecode.utterlyidle.annotations.PUT;
 import com.googlecode.utterlyidle.annotations.Path;
 import com.googlecode.utterlyidle.annotations.PathParam;
+import com.googlecode.utterlyidle.annotations.QueryParam;
 
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import static com.googlecode.barongreenback.jobs.HttpScheduler.DELAY;
 import static com.googlecode.barongreenback.jobs.HttpScheduler.INITIAL_DELAY;
+import static com.googlecode.barongreenback.jobs.HttpScheduler.INTERVAL;
+import static com.googlecode.barongreenback.jobs.HttpScheduler.JOB_ID;
 import static com.googlecode.barongreenback.jobs.HttpScheduler.REQUEST;
 import static com.googlecode.barongreenback.jobs.HttpScheduler.TIME_UNIT;
 import static com.googlecode.funclate.Model.model;
 import static com.googlecode.totallylazy.records.MapRecord.record;
-import static com.googlecode.utterlyidle.AbsoluteLocationHandler.toAbsoluteUri;
 import static com.googlecode.utterlyidle.proxy.Resource.redirect;
 import static com.googlecode.utterlyidle.proxy.Resource.resource;
 
@@ -40,21 +32,18 @@ import static com.googlecode.utterlyidle.proxy.Resource.resource;
 public class JobsResource {
     private final HttpScheduler scheduler;
     private final Request request;
-    private final BasePath basePath;
 
-    public JobsResource(HttpScheduler scheduler, Request request, BasePath basePath) {
+    public JobsResource(HttpScheduler scheduler, Request request) {
         this.scheduler = scheduler;
         this.request = request;
-        this.basePath = basePath;
     }
 
-    // http://localhost:9000/jobs/schedule/5/crawler/crawl/?id=73773c20-5bf8-4da1-865b-c44c56656c47
     @POST
     @Path("schedule/{interval}")
     public Response schedule(@PathParam("interval") Long interval, @PathParam("$") String endOfUrl) throws Exception {
         Request scheduledRequest = request.uri(request.uri().path(endOfUrl));
 
-        scheduler.schedule(scheduledRequest, record().set(INITIAL_DELAY, 0L).set(DELAY, interval).set(TIME_UNIT, TimeUnit.SECONDS));
+        scheduler.schedule(scheduledRequest, record().set(INITIAL_DELAY, 0L).set(INTERVAL, interval).set(TIME_UNIT, TimeUnit.SECONDS));
 
         return redirectToList();
     }
@@ -63,21 +52,33 @@ public class JobsResource {
         return redirect(resource(getClass()).list());
     }
 
+    @POST
+    @Path("reschedule")
+    public Response reschedule(@FormParam("id") UUID id, @FormParam("interval") Long interval) throws Exception {
+        scheduler.reschedule(id, record().set(INTERVAL, interval));
+        return redirectToList();
+    }
+
+    @GET
+    @Path("edit")
+    public Model edit(@QueryParam("id") UUID id) {
+        // TODO: handle 404
+        Record job = scheduler.job(id).get();
+        return model().add("id", id.toString()).add("interval", job.get(INTERVAL));
+    }
+
+    @POST
+    @Path("delete")
+    public Response delete(@FormParam("id") UUID id) {
+        scheduler.remove(id);
+        return redirectToList();
+    }
+
     @GET
     @Path("list")
     public Model list() {
         return model().add("jobs", jobsModel(scheduler.jobs()));
     }
-
-//    @POST
-//    @Path("queue/{method}/{uri:.+}")
-//    public Response queue(@PathParam("method") String httpMethod, @PathParam("uri") String uri) throws Exception {
-//        Request oneOffRequest = new RequestBuilder(httpMethod, toAbsoluteUri(uri, request, basePath).toString()).build();
-//
-//        Executors.newSingleThreadExecutor().submit(httpTask(oneOffRequest));
-//
-//        return Responses.response(Status.OK).entity("queued");
-//    }
 
     private List<Model> jobsModel(Sequence<Record> jobs) {
         return jobs.map(toModel()).toList();
@@ -87,7 +88,7 @@ public class JobsResource {
         return new Callable1<Record, Model>() {
             public Model call(Record record) throws Exception {
                 Request request = HttpMessageParser.parseRequest(record.get(REQUEST));
-                return model().add("method", request.method()).add("path", request.uri().path()).add("delay", record.get(DELAY));
+                return model().add("method", request.method()).add("path", request.uri().path()).add("delay", record.get(INTERVAL)).add("id", record.get(JOB_ID));
             }
         };
     }
