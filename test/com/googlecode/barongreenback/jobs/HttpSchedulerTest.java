a77a58e87086bcf40948bb7c83a974e5bbe5a79b
Daniel Worthington-Bodart <dan@bodar.com>
2011-09-19 17:18:12 +0100

    replacing mocks with stubs in http scheduler tests

--- test/com/googlecode/barongreenback/jobs/HttpSchedulerTest.java
+++ test/com/googlecode/barongreenback/jobs/HttpSchedulerTest.java
@@ -1,16 +1,12 @@
 package com.googlecode.barongreenback.jobs;
 
 import com.googlecode.totallylazy.records.Record;
-import com.googlecode.utterlyidle.Application;
-import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.RequestBuilder;
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
-import org.mockito.InOrder;
-import org.mockito.Mockito;
 
-import java.util.UUID;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -22,18 +18,12 @@ import static com.googlecode.barongreenback.jobs.HttpScheduler.TIME_UNIT;
 import static com.googlecode.totallylazy.matchers.NumberMatcher.is;
 import static com.googlecode.totallylazy.records.MapRecord.record;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 public class HttpSchedulerTest {
-    private FixedScheduler executorService = mock(FixedScheduler.class);
-    private Application application = mock(Application.class);
-    private HttpScheduler httpScheduler = new HttpScheduler(executorService, application);
-    private ScheduledFuture future = mock(ScheduledFuture.class);
     private String request = RequestBuilder.get("/test").build().toString();
     private Record schedulerSpec = record().set(REQUEST, request).set(JOB_ID, "orders").set(INITIAL_DELAY, 0L).set(INTERVAL, 10L).set(TIME_UNIT, TimeUnit.SECONDS);
+    private final StubFixedScheduler stub = new StubFixedScheduler();
+    private final HttpScheduler httpScheduler = new HttpScheduler(stub, null);
 
     @Test
     public void scheduleRequest() throws Exception {
@@ -41,32 +31,59 @@ public class HttpSchedulerTest {
 
         assertThat(httpScheduler.jobs().size(), is(1));
         assertThat(httpScheduler.job(id).get().get(INTERVAL), CoreMatchers.is(10L));
-        verify(executorService).scheduleWithFixedDelay(Mockito.any(Runnable.class), eq(0L), eq(10L), eq(TimeUnit.SECONDS));
+        assertThat(stub.initialDelay, CoreMatchers.is(0L));
+        assertThat(stub.delay, CoreMatchers.is(10L));
+        assertThat(stub.unit, CoreMatchers.is(TimeUnit.SECONDS));
     }
 
     @Test
     public void rescheduleRequest() throws Exception {
-        Mockito.when(executorService.scheduleWithFixedDelay(Mockito.any(Runnable.class), eq(0L), eq(10L), eq(TimeUnit.SECONDS))).thenReturn(future);
         String id = httpScheduler.schedule(schedulerSpec);
+
+        assertThat(stub.initialDelay, CoreMatchers.is(0L));
+        assertThat(stub.delay, CoreMatchers.is(10L));
+        assertThat(stub.unit, CoreMatchers.is(TimeUnit.SECONDS));
+
 
         httpScheduler.schedule(schedulerSpec.set(INTERVAL, 20L));
 
         assertThat(httpScheduler.jobs().size(), is(1));
         assertThat(httpScheduler.job(id).get().get(INTERVAL), CoreMatchers.is(20L));
-
-        InOrder inOrder = inOrder(executorService);
-        inOrder.verify(executorService).scheduleWithFixedDelay(Mockito.any(Runnable.class), eq(0L), eq(10L), eq(TimeUnit.SECONDS));
-        inOrder.verify(executorService).scheduleWithFixedDelay(Mockito.any(Runnable.class), eq(0L), eq(20L), eq(TimeUnit.SECONDS));
+        assertThat(stub.delay, CoreMatchers.is(20L));
     }
 
     @Test
     public void removeScheduledJob() throws Exception {
-        Mockito.when(executorService.scheduleWithFixedDelay(Mockito.any(Runnable.class), eq(0L), eq(10L), eq(TimeUnit.SECONDS))).thenReturn(future);
         String id = httpScheduler.schedule(schedulerSpec);
-
         httpScheduler.remove(id);
-
         assertThat(httpScheduler.jobs().size(), is(0));
+    }
+
+    private static class StubFixedScheduler implements FixedScheduler {
+        public long initialDelay;
+        public long delay;
+        public TimeUnit unit;
+
+        public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, final long delay, TimeUnit unit) {
+            this.initialDelay = initialDelay;
+            this.delay = delay;
+            this.unit = unit;
+            return new DougDoesNotLikeTests(command);
+        }
+    }
+
+    private static class DougDoesNotLikeTests<T> extends FutureTask<T> implements ScheduledFuture<T> {
+        public DougDoesNotLikeTests(Runnable command) {
+            super(command, null);
+        }
+
+        public long getDelay(TimeUnit unit) {
+            return 0;
+        }
+
+        public int compareTo(Delayed o) {
+            return 0;
+        }
     }
 
 }
