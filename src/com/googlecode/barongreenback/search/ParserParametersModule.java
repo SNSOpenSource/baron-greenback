3a98eda173224906cea6bec28d5942701b910d64
Mateusz Kwasniewski <kwasniew@users.noreply.github.com>
2011-11-17 11:27:48 +0000

    Exposing container in ParserParametersModule

--- src/com/googlecode/barongreenback/search/ParserParametersModule.java
+++ src/com/googlecode/barongreenback/search/ParserParametersModule.java
@@ -2,7 +2,8 @@ package com.googlecode.barongreenback.search;
 
 import com.googlecode.barongreenback.search.parser.ParserParameters;
 import com.googlecode.utterlyidle.modules.Module;
+import com.googlecode.yadic.Container;
 
 public interface ParserParametersModule extends Module {
-    ParserParameters addParameters(ParserParameters parameters);
+    ParserParameters addParameters(ParserParameters parameters, Container container);
 }
