528c8e97279577aa76e3dc6d2824d3b20c61017a
Daniel Worthington-Bodart <dan@bodar.com>
2011-11-18 17:20:19 +0000

    Removed unneed parenthesis from generate queries

---
+++ src/com/googlecode/barongreenback/search/SearchModule.java
@@ -0,0 +1,35 @@
+package com.googlecode.barongreenback.search;
+
+import com.googlecode.barongreenback.search.parser.ParametrizedParser;
+import com.googlecode.barongreenback.search.parser.PredicateParser;
+import com.googlecode.barongreenback.search.parser.StandardParser;
+import com.googlecode.barongreenback.search.parser.ParserParameters;
+import com.googlecode.utterlyidle.Resources;
+import com.googlecode.utterlyidle.modules.Module;
+import com.googlecode.utterlyidle.modules.ModuleDefiner;
+import com.googlecode.utterlyidle.modules.ModuleDefinitions;
+import com.googlecode.utterlyidle.modules.RequestScopedModule;
+import com.googlecode.utterlyidle.modules.ResourcesModule;
+import com.googlecode.yadic.Container;
+import org.apache.lucene.queryParser.ParseException;
+
+import static com.googlecode.utterlyidle.annotations.AnnotatedBindings.annotatedClass;
+
+public class SearchModule implements ResourcesModule, RequestScopedModule, ModuleDefiner {
+    public Module addResources(Resources resources) throws ParseException {
+        resources.add(annotatedClass(SearchResource.class));
+        return this;
+    }
+
+    public Module addPerRequestObjects(Container container) throws Exception {
+        container.add(PredicateParser.class, StandardParser.class);
+        container.decorate(PredicateParser.class, ParametrizedParser.class);
+        container.add(ParserParameters.class);
+        return this;
+    }
+
+    public Module defineModules(ModuleDefinitions moduleDefinitions) throws Exception {
+        moduleDefinitions.addRequestModule(ParserParametersModule.class);
+        return this;
+    }
+}
---
+++ test/com/googlecode/barongreenback/search/ViewSearchPage.java
@@ -0,0 +1,34 @@
+package com.googlecode.barongreenback.search;
+
+import com.googlecode.utterlyidle.Status;
+import com.googlecode.utterlyidle.html.Html;
+import com.googlecode.utterlyidle.HttpHandler;
+import com.googlecode.utterlyidle.Response;
+
+import static com.googlecode.totallylazy.proxy.Call.method;
+import static com.googlecode.totallylazy.proxy.Call.on;
+import static com.googlecode.utterlyidle.RequestBuilder.get;
+import static com.googlecode.utterlyidle.annotations.AnnotatedBindings.relativeUriOf;
+import static java.lang.String.format;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+public class ViewSearchPage {
+    private final HttpHandler httpHandler;
+    private final Html html;
+
+    public ViewSearchPage(HttpHandler httpHandler, String view, String query) throws Exception {
+        this(httpHandler, httpHandler.handle(get("/" + relativeUriOf(method(on(SearchResource.class).list(view, query)))).build()));
+    }
+
+    public ViewSearchPage(HttpHandler httpHandler, Response response) throws Exception {
+        this.httpHandler = httpHandler;
+        this.html = Html.html(response);
+        assertThat(html.title(), containsString("Search"));
+    }
+
+    public boolean containsCell(String value, String cssClass) {
+        return html.selectContent(format("//td[@class='%s']", cssClass)).contains(value);
+    }
+
+}
