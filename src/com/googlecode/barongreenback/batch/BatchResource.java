7263bc5a300582dc96e8ab4f67f147c515fcd2cf
Mateusz Kwasniewski <kwasniew@users.noreply.github.com>
2011-11-29 16:49:58 +0000

    adding job wrapper around job record

---
+++ src/com/googlecode/barongreenback/batch/BatchResource.java
@@ -0,0 +1,84 @@
+package com.googlecode.barongreenback.batch;
+
+import com.googlecode.barongreenback.shared.ModelRepository;
+import com.googlecode.funclate.Model;
+import com.googlecode.funclate.json.Json;
+import com.googlecode.totallylazy.Callable2;
+import com.googlecode.totallylazy.Pair;
+import com.googlecode.totallylazy.Predicates;
+import com.googlecode.totallylazy.records.Record;
+import com.googlecode.utterlyidle.MediaType;
+import com.googlecode.utterlyidle.Redirector;
+import com.googlecode.utterlyidle.Response;
+import com.googlecode.utterlyidle.annotations.*;
+
+import java.util.*;
+
+import static com.googlecode.funclate.Model.model;
+import static com.googlecode.totallylazy.proxy.Call.method;
+import static com.googlecode.totallylazy.proxy.Call.on;
+
+@Path("batch")
+@Produces(MediaType.TEXT_HTML)
+public class BatchResource {
+
+    private ModelRepository modelRepository;
+    private Redirector redirector;
+
+    public BatchResource(ModelRepository modelRepository, Redirector redirector) {
+        this.modelRepository = modelRepository;
+        this.redirector = redirector;
+    }
+
+    @GET
+    @Path("import")
+    public Model operations() {
+        return model();
+    }
+
+    @GET
+    @Path("import")
+    public Model operations(@QueryParam("message") String message, @QueryParam("category") String category) {
+        return messageModel(message, category);
+    }
+
+    @GET
+    @Path("export")
+    @Produces("application/json")
+    public String export() {
+        Map<String, Object> map = modelRepository.find(Predicates.<Record>all()).fold(new HashMap<String, Object>(), addUuidAndModel());
+        return Json.toJson(map);
+    }
+
+    @POST
+    @Path("import")
+    public Object importJson(@FormParam("model") String batchModel) {
+        try {
+            Map<String, Object> uuidsAndModels = Json.parse(batchModel);
+            for (Map.Entry<String, Object> entry : uuidsAndModels.entrySet()) {
+                modelRepository.set(UUID.fromString(entry.getKey()), Model.fromMap((Map<String, Object>) entry.getValue()));
+            }
+            return redirectWithMessage(String.format("Imported %s items", uuidsAndModels.size()), "success");
+        } catch (Exception e) {
+            return messageModel(String.format("Import error: %s", e.getMessage()), "error").add("model", batchModel);
+        }
+
+    }
+
+    private Model messageModel(String message, String category) {
+        return model().add("message", model().add("text", message).add("category", category));
+    }
+
+    private Response redirectWithMessage(String text, String category) {
+        return redirector.seeOther(method(on(BatchResource.class).operations(text, category)));
+    }
+
+    private Callable2<? super Map<String, Object>, ? super Pair<UUID, Model>, Map<String, Object>> addUuidAndModel() {
+        return new Callable2<Map<String, Object>, Pair<UUID, Model>, Map<String, Object>>() {
+            public Map<String, Object> call(Map<String, Object> map, Pair<UUID, Model> pair) throws Exception {
+                map.put(pair.first().toString(), pair.second());
+                return map;
+            }
+        };
+    }
+}
---
+++ test/com/googlecode/barongreenback/search/SearchResourceTest.java
@@ -0,0 +1,83 @@
+package com.googlecode.barongreenback.search;
+
+import com.googlecode.barongreenback.crawler.CrawlerTest;
+import com.googlecode.barongreenback.crawler.CrawlerTests;
+import com.googlecode.barongreenback.shared.ApplicationTests;
+import com.googlecode.barongreenback.shared.ModelRepository;
+import com.googlecode.barongreenback.views.Views;
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Sequence;
+import com.googlecode.totallylazy.Uri;
+import com.googlecode.totallylazy.records.Keyword;
+import com.googlecode.totallylazy.records.Record;
+import com.googlecode.totallylazy.records.lucene.LuceneRecords;
+import com.googlecode.totallylazy.regex.Matches;
+import com.googlecode.utterlyidle.Server;
+import com.googlecode.utterlyidle.httpserver.RestServer;
+import com.googlecode.yadic.Container;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static com.googlecode.totallylazy.Runnables.VOID;
+import static com.googlecode.totallylazy.matchers.NumberMatcher.is;
+import static com.googlecode.totallylazy.records.Keywords.keyword;
+import static com.googlecode.totallylazy.records.Keywords.keywords;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SearchResourceTest extends ApplicationTests {
+     @Test
+    public void handlesInvalidQueriesInANiceWay() throws Exception {
+        SearchPage searchPage = new SearchPage(browser, "users", "^&%$^%");
+        assertThat(searchPage.queryMessage(), Matchers.is("Invalid Query"));
+    }
+
+     @Test
+    public void supportsDelete() throws Exception {
+        SearchPage searchPage = new SearchPage(browser, "users", "", true);
+        assertThat(searchPage.numberOfResults(), is(2));
+        searchPage = searchPage.delete();
+        assertThat(searchPage.numberOfResults(), is(0));
+    }
+
+    @Test
+    public void supportsQueryAll() throws Exception {
+        SearchPage searchPage = new SearchPage(browser, "users", "");
+        assertThat(searchPage.numberOfResults(), is(2));
+    }
+
+    @Test
+    public void supportsQueryForAParticularEntry() throws Exception {
+        SearchPage searchPage = new SearchPage(browser, "users", "id:\"urn:uuid:c356d2c5-f975-4c4d-8e2a-a698158c6ef1\"");
+        assertThat(searchPage.numberOfResults(), is(1));
+    }
+
+    @Test
+    public void whenAnUnknownViewIsSpecifiedThenNoResultsShouldBeShown() throws Exception {
+        SearchPage searchPage = new SearchPage(browser, "UNKNOWN", "");
+        assertThat(searchPage.numberOfResults(), is(0));
+    }
+
+    @Before
+    public void addSomeData() throws Exception {
+        RestServer server = CrawlerTests.setupServerWithDataFeed();
+
+        final Sequence<Record> recordSequence = CrawlerTest.crawlOnePageOnly().realise();
+
+        application.usingRequestScope(new Callable1<Container, Void>() {
+            public Void call(Container container) throws Exception {
+                LuceneRecords luceneRecords = container.get(LuceneRecords.class);
+                Keyword<Object> users = keyword("users");
+                luceneRecords.define(users, keywords(recordSequence).toArray(Keyword.class));
+                luceneRecords.add(users, recordSequence);
+                ModelRepository views = container.get(ModelRepository.class);
+                views.set(UUID.randomUUID(), Views.convertToViewModel(users, keywords(recordSequence)));
+                return VOID;
+            }
+        });
+
+        server.close();
+    }
+}
