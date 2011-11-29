9fe8b0b6709f22f7c314887844da0d714c20aa7e
Daniel Worthington-Bodart <dan@bodar.com>
2011-11-29 16:05:24 +0000

    Updated to use faster lucene records

--- src/com/googlecode/barongreenback/lucene/LuceneModule.java
+++ src/com/googlecode/barongreenback/lucene/LuceneModule.java
@@ -3,6 +3,8 @@ package com.googlecode.barongreenback.lucene;
 import com.googlecode.funclate.Model;
 import com.googlecode.totallylazy.records.Records;
 import com.googlecode.totallylazy.records.lucene.LuceneRecords;
+import com.googlecode.totallylazy.records.lucene.LuceneStorage;
+import com.googlecode.totallylazy.records.lucene.OptimisedStorage;
 import com.googlecode.totallylazy.records.lucene.mappings.Mappings;
 import com.googlecode.utterlyidle.modules.ApplicationScopedModule;
 import com.googlecode.utterlyidle.modules.Module;
@@ -16,18 +18,17 @@ import java.io.PrintStream;
 
 public class LuceneModule implements ApplicationScopedModule, RequestScopedModule{
     public Module addPerApplicationObjects(Container container) {
-        container.addInstance(Version.class, Version.LUCENE_33);
         container.add(LuceneIndexConfiguration.class);
-        container.addActivator(IndexWriter.class, IndexWriterActivator.class);
         container.addActivator(Directory.class, DirectoryActivator.class);
+        container.add(LuceneStorage.class, OptimisedStorage.class);
         return this;
     }
 
     public Module addPerRequestObjects(Container container) {
         container.add(LuceneRecords.class);
+        container.addActivator(Records.class, container.getActivator(LuceneRecords.class));
         container.addInstance(Mappings.class, new Mappings().add(Model.class, new ModelMapping()));
         container.addInstance(PrintStream.class, new StringPrintStream());
-        container.addActivator(Records.class, container.getActivator(LuceneRecords.class));
         return this;
     }
 }
