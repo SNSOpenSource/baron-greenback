78e30c9bc16e0c8e016c0f3b1b2c3f665c5fd001
Jozef Dransfield <Jozef.Dransfield@me.com>
2011-12-20 15:57:33 +0000

    the shortcut action now either sends you to the unique page for a search

---
+++ src/com/googlecode/barongreenback/crawler/Crawler.java
@@ -0,0 +1,40 @@
+package com.googlecode.barongreenback.crawler;
+
+import com.googlecode.barongreenback.shared.RecordDefinition;
+import com.googlecode.totallylazy.Sequence;
+import com.googlecode.totallylazy.Uri;
+import com.googlecode.totallylazy.records.Keyword;
+import com.googlecode.totallylazy.records.Keywords;
+import com.googlecode.totallylazy.records.Record;
+import com.googlecode.utterlyidle.HttpHandler;
+import com.googlecode.utterlyidle.handlers.AuditHandler;
+import com.googlecode.utterlyidle.handlers.ClientHttpHandler;
+import com.googlecode.utterlyidle.handlers.HttpClient;
+import com.googlecode.utterlyidle.handlers.PrintAuditor;
+
+import java.util.Date;
+
+import static com.googlecode.totallylazy.records.Keywords.keyword;
+
+
+public class Crawler {
+    public static final Keyword<String> MORE = keyword("more", String.class);
+    public static final Keyword<Boolean> CHECKPOINT = Keywords.keyword("checkpoint", Boolean.class);
+    public static final Keyword<Date> CHECKPOINT_VALUE = keyword("checkpointValue", Date.class);
+
+    private final HttpHandler client;
+
+    public Crawler() {
+        this(new ClientHttpHandler());
+    }
+
+    public Crawler(HttpClient client) {
+        this.client = new AuditHandler(client, new PrintAuditor(System.out));
+    }
+
+    public Sequence<Record> crawl(Uri uri, String more, Object checkpoint, RecordDefinition recordDefinition) throws Exception {
+        Feeder<Uri> feeder = new SubFeeder(new DuplicateRemover(new CheckPointStopper(checkpoint, new UriFeeder(client, more))));
+        return feeder.get(uri, recordDefinition).
+                memorise();
+    }
+}
