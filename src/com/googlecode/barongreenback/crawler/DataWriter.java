12b20fa7299c1c299cedadbf9428e84e9a6dab1c
Raymond Barlow <raymanoz@users.noreply.github.com>
2012-07-02 16:56:58 +0100

    Added a check for a single unique field in the crawler definition. Fixed some styling issues in expandable frames.

--- src/com/googlecode/barongreenback/crawler/DataWriter.java
+++ src/com/googlecode/barongreenback/crawler/DataWriter.java
@@ -7,6 +7,7 @@ import com.googlecode.utterlyidle.Application;
 import com.googlecode.utterlyidle.Response;
 import com.googlecode.yadic.Container;
 
+import java.io.PrintStream;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.googlecode.barongreenback.shared.RecordDefinition.uniqueFields;
@@ -16,25 +17,27 @@ import static com.googlecode.totallylazy.Predicates.*;
 
 public class DataWriter {
     private final Records records;
+    private final PrintStream log;
 
-    public DataWriter(BaronGreenbackRecords records) {
-        this(records.value());
+    public DataWriter(BaronGreenbackRecords records, PrintStream log) {
+        this(records.value(), log);
     }
 
-    public DataWriter(Records records) {
+    public DataWriter(Records records, PrintStream log) {
         this.records = records;
+        this.log = log;
     }
 
     public Number writeUnique(final Definition destination, final Sequence<Record> newRecords) {
         if (newRecords.isEmpty()) return 0;
 
         try {
-            Sequence<Keyword<?>> unique = uniqueFields(destination);
+            Sequence<Keyword<?>> unique = checkOnlyOne(uniqueFields(destination));
             if(newRecords.head().fields().map(Callables.<Keyword<?>>first()).exists(in(unique))) {
                 return records.put(destination, Record.methods.update(using(unique), newRecords));
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(log);
             throw new RuntimeException(e);
         }
 
@@ -49,7 +52,7 @@ public class DataWriter {
                     @Override
                     public Number call(Container container) throws Exception {
                         try {
-                            Number updated = new DataWriter(container.get(BaronGreenbackRecords.class).value()).writeUnique(job.destination(), newData);
+                            Number updated = new DataWriter(container.get(BaronGreenbackRecords.class).value(), container.get(PrintStream.class)).writeUnique(job.destination(), newData);
                             job.container().get(AtomicInteger.class).addAndGet(updated.intValue());
                             return updated;
                         } catch (Exception e) {
@@ -60,5 +63,13 @@ public class DataWriter {
                 });
             }
         };
+    }
+
+    private Sequence<Keyword<?>> checkOnlyOne(Sequence<Keyword<?>> uniques) {
+        if(uniques.size() != 1) {
+            throw new IllegalStateException("There should be exactly 1 unique field, instead there are " + uniques.size() + " (" + uniques + ").\n" +
+                    "Please correct the crawler definition.");
+        }
+        return uniques;
     }
 }
--- test/com/googlecode/barongreenback/crawler/DataWriterTest.java
+++ test/com/googlecode/barongreenback/crawler/DataWriterTest.java
@@ -1,5 +1,6 @@
 package com.googlecode.barongreenback.crawler;
 
+import com.googlecode.barongreenback.persistence.StringPrintStream;
 import com.googlecode.lazyrecords.*;
 import com.googlecode.lazyrecords.memory.MemoryRecords;
 import com.googlecode.totallylazy.Sequence;
@@ -12,31 +13,37 @@ import static com.googlecode.lazyrecords.Definition.constructors.definition;
 import static com.googlecode.lazyrecords.Record.constructors.record;
 import static com.googlecode.totallylazy.Sequences.one;
 import static com.googlecode.totallylazy.Sequences.sequence;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class DataWriterTest {
-    @Test
-    public void shouldNotWriteDataToRecordsWhenNotUnique() throws Exception {
-        Records records = new MemoryRecords();
-        Keyword<String> name = Keywords.keyword("name", String.class);
-        Definition children = definition("children", name);
-        Record expected = record().set(name, "Dan");
-        new DataWriter(records).writeUnique(children, one(expected));
-        Sequence<Record> result = records.get(children);
-        assertThat(result, is(Sequences.<Record>empty()));
-    }
-
     @Test
     public void shouldWriteUniqueDataToRecords() throws Exception {
         Records records = new MemoryRecords();
         Keyword<String> name = Keywords.keyword("name", String.class).metadata(record().set(Keywords.UNIQUE, true));
         Definition children = definition("children", name);
         Record expected = record().set(name, "Dan");
-        DataWriter dataWriter = new DataWriter(records);
+        DataWriter dataWriter = new DataWriter(records, new StringPrintStream());
         dataWriter.writeUnique(children, one(expected));
         dataWriter.writeUnique(children, one(expected));
         Sequence<Record> result = records.get(children);
         assertThat(result, is(one(expected)));
+    }
+
+    @Test
+    public void shouldBeExactlyOneUniqueField() {
+        Records records = new MemoryRecords();
+        Keyword<String> field1 = Keywords.keyword("field1", String.class).metadata(record().set(Keywords.UNIQUE, true));
+        Keyword<String> field2 = Keywords.keyword("field2", String.class).metadata(record().set(Keywords.UNIQUE, true));
+        Definition children = definition("children", field1, field2);
+        Record record = record().set(field1, "some value").set(field2, "some other value");
+        try {
+            new DataWriter(records, new StringPrintStream()).writeUnique(children, one(record));
+            fail("An " + IllegalStateException.class.getSimpleName() + " exception should have been thrown");
+        } catch (Exception e) {
+            assertThat(e.getCause(), instanceOf(IllegalStateException.class));
+        }
     }
 }
