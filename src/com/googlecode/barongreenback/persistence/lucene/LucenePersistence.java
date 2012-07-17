fd405ec259ada4c38830b7108d6e308c1dd5049f
Daniel Worthington-Bodart <dan@bodar.com>
2012-07-17 15:43:47 +0100

    Moved Lucene backup and restore to LazyRecords

--- src/com/googlecode/barongreenback/persistence/lucene/LucenePersistence.java
+++ src/com/googlecode/barongreenback/persistence/lucene/LucenePersistence.java
@@ -20,13 +20,9 @@ import static com.googlecode.totallylazy.Zip.zip;
 
 public class LucenePersistence implements Persistence {
     private final LuceneStorage luceneStorage;
-    private final Directory directory;
-    private final SearcherPool searcherPool;
 
-    public LucenePersistence(LuceneStorage luceneStorage, Directory directory, SearcherPool searcherPool) {
+    public LucenePersistence(LuceneStorage luceneStorage) {
         this.luceneStorage = luceneStorage;
-        this.directory = directory;
-        this.searcherPool = searcherPool;
     }
 
     public static String luceneDirectory(File file) {
@@ -48,57 +44,11 @@ public class LucenePersistence implements Persistence {
 
     @Override
     public void backup(File file) throws Exception {
-        File folder = unzippedName(file);
-        Files.delete(folder);
-        using(directoryFor(folder), copy().apply(directory));
-        zip(folder, file);
-        Files.delete(folder);
+        luceneStorage.backup(file);
     }
 
     @Override
     public void restore(File source) throws Exception {
-        luceneStorage.deleteAll();
-        deleteAllSegments(directory);
-        using(directoryFor(unzipIfNeeded(source)), copy().flip().apply(directory));
+        luceneStorage.restore(source);
     }
-
-    private File unzipIfNeeded(File source) throws IOException {
-        if (source.isFile()) {
-            File unzipped = unzippedName(source);
-            unzip(source, unzipped);
-            return unzipped;
-        }
-        return source;
-    }
-
-    private void deleteAllSegments(Directory directory) throws IOException {
-        for (String segment : directory.listAll()) {
-            directory.deleteFile(segment);
-        }
-    }
-
-    private Directory directoryFor(File file) throws IOException {
-        return new NIOFSDirectory(file);
-    }
-
-    public static Function2<Directory, Directory, Void> copy() {
-        return new Function2<Directory, Directory, Void>() {
-            @Override
-            public Void call(Directory source, Directory destination) throws Exception {
-                copy(source, destination);
-                return VOID;
-            }
-        };
-    }
-
-    public static void copy(Directory source, Directory destination) throws IOException {
-        for (String segment : source.listAll()) {
-            source.copy(destination, segment, segment);
-        }
-    }
-
-    private File unzippedName(File file) {
-        return new File(file.toString() + ".unzipped");
-    }
-
 }
