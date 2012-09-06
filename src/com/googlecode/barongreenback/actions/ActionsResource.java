21c6de762e872ef3adf6d578b5159b861da42598
spmiller <spmiller@users.noreply.github.com>
2012-09-06 13:59:19 +0100

    Add method for deleting specific ids from a view

--- src/com/googlecode/barongreenback/actions/ActionsResource.java
+++ src/com/googlecode/barongreenback/actions/ActionsResource.java
@@ -3,6 +3,8 @@ package com.googlecode.barongreenback.actions;
 import com.googlecode.barongreenback.search.RecordsService;
 import com.googlecode.barongreenback.search.SearchResource;
 import com.googlecode.barongreenback.shared.AdvancedMode;
+import com.googlecode.funclate.Model;
+import com.googlecode.totallylazy.Strings;
 import com.googlecode.utterlyidle.MediaType;
 import com.googlecode.utterlyidle.Redirector;
 import com.googlecode.utterlyidle.Response;
@@ -12,6 +14,8 @@ import com.googlecode.utterlyidle.annotations.PathParam;
 import com.googlecode.utterlyidle.annotations.Produces;
 import com.googlecode.utterlyidle.annotations.QueryParam;
 
+import static com.googlecode.barongreenback.views.ViewsRepository.unwrap;
+import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.totallylazy.proxy.Call.method;
 import static com.googlecode.totallylazy.proxy.Call.on;
 
@@ -38,4 +42,15 @@ public class ActionsResource {
         recordsService.delete(viewName, query);
         return redirector.seeOther(method(on(SearchResource.class).list(viewName, query)));
     }
+
+
+    @POST
+    @Path("delete")
+    public Response delete(@PathParam("view") String viewName, @QueryParam("query") String query, @QueryParam("id") Iterable<String> id) {
+        String idName = unwrap(recordsService.view(viewName)).get("keywords", Model.class).get("name", String.class);
+        delete(viewName, sequence(id).map(Strings.format(idName + ":%s")).toString(" OR "));
+        return redirector.seeOther(method(on(SearchResource.class).list(viewName, query)));
+    }
+
+
 }
