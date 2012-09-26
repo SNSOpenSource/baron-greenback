649126fffa4e99888cab87c265003f5246101a9d
Daniel Worthington-Bodart <dan@bodar.com>
2012-09-26 14:35:57 +0100

    changed deafult for processing queue capacity to 1000

--- src/com/googlecode/barongreenback/crawler/executor/CrawlerExecutorsActivator.java
+++ src/com/googlecode/barongreenback/crawler/executor/CrawlerExecutorsActivator.java
@@ -19,7 +19,7 @@ public class CrawlerExecutorsActivator implements Callable<CrawlerExecutors> {
         Integer inputHandlerThreads = getProperty("input.handler.threads", "10");
         Integer inputHandlerCapacity = getProperty("input.handler.capacity", "0");
         Integer processHandlerThreads = getProperty("process.handler.threads", "10");
-        Integer processHandlerCapacity = getProperty("process.handler.capacity", "0");
+        Integer processHandlerCapacity = getProperty("process.handler.capacity", "1000");
         Integer outputHandlerThreads = getProperty("output.handler.threads", "1");
         Integer outputHandlerCapacity = getProperty("output.handler.capacity", "1000");
 
