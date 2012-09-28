a51886f03e84ad5e3f65d4331508b1a194bfe2da
Tom <Tom@localhost>
2012-09-28 15:38:21 +0100

    moved select buttons to right of page

--- src/com/googlecode/barongreenback/shared/actions/actions.js
+++ src/com/googlecode/barongreenback/shared/actions/actions.js
@@ -22,16 +22,17 @@ jQuery(document).ready(function() {
             jQuery('.actions .message').text(jQuery('table.results tr.selected').size() + " rows are selected");
         }
     });
-    jQuery('.actions a.selectPage').click(function() {
+    jQuery('.selectors a.selectPage').click(function() {
         jQuery('table.results tbody tr').removeClass('selected');
         jQuery('table.results tbody td:first-child').click();
         return false;
     });
-    jQuery('.actions a.selectAll').click(function() {
-        jQuery('.actions a.selectPage').click();
+    jQuery('.selectors a.selectAll').click(function() {
+        jQuery('.selectors a.selectPage').click();
         BGB.search.bulk.actions.removeAllIdentifiers();
         jQuery('div.actions form').append('<input type="hidden" name="query" value="' + jQuery('meta[name="query"]').attr('content') + '">');
-        jQuery('.actions .message').text('ALL ' + BGB.search.rowCount() +' rows are selected');
+        jQuery('.actions .message').text('ALL ' + BGB.search.rowCount() +' rows are selected');b
+
         return false;
     });
 });
