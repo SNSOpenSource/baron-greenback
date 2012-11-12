043663ff0aa5c8236ac8ba01c32ef53efca316cd
Daniel Worthington-Bodart <dan@bodar.com>
2012-11-12 16:13:06 +0000

    Added support for executors per definition and http authority

--- src/com/googlecode/barongreenback/crawler/executor/CrawlerExecutors.java
+++ src/com/googlecode/barongreenback/crawler/executor/CrawlerExecutors.java
@@ -4,6 +4,10 @@ import com.googlecode.barongreenback.crawler.BlockingRetryRejectedExecutionHandl
 import com.googlecode.barongreenback.crawler.DataWriter;
 import com.googlecode.barongreenback.crawler.StagedJob;
 import com.googlecode.barongreenback.crawler.StatusMonitor;
+import com.googlecode.lazyrecords.Definition;
+import com.googlecode.totallylazy.Callables;
+import com.googlecode.totallylazy.Callers;
+import com.googlecode.totallylazy.Lazy;
 import com.googlecode.totallylazy.Maps;
 import com.googlecode.totallylazy.Pair;
 import com.googlecode.totallylazy.Sequence;
@@ -16,6 +20,7 @@ import java.io.Closeable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -26,14 +31,17 @@ import static com.googlecode.barongreenback.crawler.executor.CrawlerConfigValues
 import static com.googlecode.barongreenback.crawler.executor.CrawlerConfigValues.OUTPUT_HANDLER_THREADS;
 import static com.googlecode.barongreenback.crawler.executor.CrawlerConfigValues.PROCESS_HANDLER_CAPACITY;
 import static com.googlecode.barongreenback.crawler.executor.CrawlerConfigValues.PROCESS_HANDLER_THREADS;
+import static com.googlecode.totallylazy.Callables.value;
 import static com.googlecode.totallylazy.Closeables.safeClose;
+import static com.googlecode.totallylazy.Lazy.lazy;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.totallylazy.Unchecked.cast;
 
 public class CrawlerExecutors implements Closeable {
-    private JobExecutor inputHandler;
-    private JobExecutor processHandler;
-    private DataWriter outputHandler;
+
+    private ConcurrentHashMap<String, Lazy<JobExecutor>> inputHandler = new ConcurrentHashMap<String, Lazy<JobExecutor>>();
+    private ConcurrentHashMap<Definition, Lazy<JobExecutor>> processHandler = new ConcurrentHashMap<Definition, Lazy<JobExecutor>>();
+    private ConcurrentHashMap<Definition, Lazy<DataWriter>> outputHandler = new ConcurrentHashMap<Definition, Lazy<DataWriter>>();
     private Map<CrawlerConfigValues, Integer> configValues = new HashMap<CrawlerConfigValues, Integer>();
     private final Application application;
 
@@ -46,18 +54,25 @@ public class CrawlerExecutors implements Closeable {
         configValues.put(PROCESS_HANDLER_CAPACITY, processHandlerCapacity);
         configValues.put(OUTPUT_HANDLER_THREADS, outputHandlerThreads);
         configValues.put(OUTPUT_HANDLER_CAPACITY, outputHandlerCapacity);
-        initialise();
     }
 
-    private void initialise() {
-        this.inputHandler = jobExecutor(configValues.get(INPUT_HANDLER_THREADS), configValues.get(INPUT_HANDLER_CAPACITY), "Incoming");
-        this.processHandler = jobExecutor(configValues.get(PROCESS_HANDLER_THREADS), configValues.get(PROCESS_HANDLER_CAPACITY), "Processing");
-        this.outputHandler = createDataWriter(configValues.get(OUTPUT_HANDLER_THREADS), configValues.get(OUTPUT_HANDLER_CAPACITY), 1, "Writing");
+    private DataWriter outputExecutor(String suffix) {
+        return createDataWriter(configValues.get(OUTPUT_HANDLER_THREADS), configValues.get(OUTPUT_HANDLER_CAPACITY), 1, "Writing " + suffix);
+    }
+
+    private ThreadPoolJobExecutor processExecutor(String suffix) {
+        return jobExecutor(configValues.get(PROCESS_HANDLER_THREADS), configValues.get(PROCESS_HANDLER_CAPACITY), "Processing " + suffix);
+    }
+
+    private ThreadPoolJobExecutor inputExecutor(String suffix) {
+        return jobExecutor(configValues.get(INPUT_HANDLER_THREADS), configValues.get(INPUT_HANDLER_CAPACITY), "Reading " + suffix);
     }
 
     public void resetExecutors() {
         close();
-        initialise();
+        inputHandler.clear();
+        processHandler.clear();
+        outputHandler.clear();
     }
 
     public Integer inputHandlerThreads() {
@@ -85,19 +100,44 @@ public class CrawlerExecutors implements Closeable {
     }
 
     public JobExecutor inputHandler(StagedJob job) {
-        return inputHandler;
+        final String authority = job.datasource().uri().authority();
+        inputHandler.putIfAbsent(authority, new Lazy<JobExecutor>(){
+            @Override
+            protected JobExecutor get() throws Exception {
+                return inputExecutor(authority);
+            }
+        });
+        return inputHandler.get(authority).value();
     }
 
     public JobExecutor processHandler(StagedJob job) {
-        return processHandler;
+        final Definition definition = job.destination();
+        processHandler.putIfAbsent(definition, new Lazy<JobExecutor>(){
+            @Override
+            protected JobExecutor get() throws Exception {
+                return processExecutor(definition.name());
+            }
+        });
+        return processHandler.get(definition).value();
     }
 
     public DataWriter outputHandler(StagedJob job) {
-        return outputHandler;
+        final Definition definition = job.destination();
+        outputHandler.putIfAbsent(definition, new Lazy<DataWriter>(){
+            @Override
+            protected DataWriter get() throws Exception {
+                return outputExecutor(definition.name());
+            }
+        });
+        return outputHandler.get(definition).value();
     }
 
-    public Sequence<StatusMonitor> statusMonitors() {
-        return Sequences.<StatusMonitor>sequence(inputHandler, processHandler, outputHandler);
+    public Sequence<JobExecutor> statusMonitors() {
+        Sequence<JobExecutor> inputHandlers = sequence(inputHandler.values()).map(value(JobExecutor.class));
+        Sequence<JobExecutor> processHandlers = sequence(processHandler.values()).map(value(JobExecutor.class));
+        Sequence<DataWriter> outputHandlers = sequence(outputHandler.values()).map(value(DataWriter.class));
+
+        return Sequences.flatten(sequence(inputHandlers, processHandlers, outputHandlers));
     }
 
     public void handlerValues(Sequence<Pair<CrawlerConfigValues, Integer>> executorValues) {
@@ -107,7 +147,7 @@ public class CrawlerExecutors implements Closeable {
 
     @Override
     public void close() {
-        sequence(inputHandler, processHandler, outputHandler).each(safeClose());
+        statusMonitors().each(safeClose());
     }
 
     private static ThreadPoolExecutor executor(int threads, int capacity, Class<? extends BlockingQueue> queueClass) {
