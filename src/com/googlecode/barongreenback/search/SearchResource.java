04d9bead222357c7de253b8f7f222ae9e4aefbd3
Daniel Worthington-Bodart <dan@bodar.com>
2013-05-02 13:39:42 +0100

    Updated LR:236

--- src/com/googlecode/barongreenback/search/SearchResource.java
+++ src/com/googlecode/barongreenback/search/SearchResource.java
@@ -4,7 +4,7 @@ import com.googlecode.barongreenback.shared.pager.Pager;
 import com.googlecode.barongreenback.shared.sorter.Sorter;
 import com.googlecode.barongreenback.views.ViewsRepository;
 import com.googlecode.funclate.Model;
-import com.googlecode.lazyrecords.CsvWriter;
+import com.googlecode.lazyrecords.csv.CsvWriterLegacy;
 import com.googlecode.lazyrecords.Definition;
 import com.googlecode.lazyrecords.Keyword;
 import com.googlecode.lazyrecords.Keywords;
@@ -112,7 +112,7 @@ public class SearchResource {
                         using(new OutputStreamWriter(new BufferedOutputStream(outputStream, 32768)), new Block<OutputStreamWriter>() {
                             @Override
                             public void execute(OutputStreamWriter writer) throws Exception {
-                                CsvWriter.writeTo(result, writer, visibleHeaders);
+                                CsvWriterLegacy.writeTo(result, writer, visibleHeaders);
                             }
                         });
                     }
