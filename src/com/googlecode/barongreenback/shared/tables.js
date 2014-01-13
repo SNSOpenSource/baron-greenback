9bea18a18d8a8c89bbefde5bab41696b07a45fcb
Stuart & Enrico <spmiller@users.noreply.github.com>
2014-01-13 16:03:46 +0000

    Fixing table sorting using datatables column types detection and improved logic for fixed header positioning.

--- src/com/googlecode/barongreenback/shared/tables.js
+++ src/com/googlecode/barongreenback/shared/tables.js
@@ -1,77 +1,156 @@
-$(document).ready(function() {
-    if($('#results > table').length == 0) {
-        return;
+BGB.namespace('tables.dataTables').init = function () {
+    $.fn.dataTableExt.oSort['string-pre'] = function (value) {
+        if (typeof value != "string")  value = value !== null && value.toString ? value.toString() : "";
+        return value;
     }
 
-    $.fn.dataTableExt.oSort['string-pre'] = function (h) {
-        if(typeof h != "string")  h = h !== null && h.toString ? h.toString() : "";
-        return h;
-    }
+    Date.setLocale("en-UK");
+
+    $.fn.dataTableExt.aTypes.unshift(function (sData) {
+        if (sData !== null && !sData.toString().match(/^\d+$/) && Date.create && Date.create(sData).isValid()) {
+            return 'sugar';
+        }
+        return null;
+    });
+
+    jQuery.fn.dataTableExt.aTypes.unshift(function (sData) {
+        sData = typeof sData.replace == 'function' ?
+            sData.replace(/<[\s\S]*?>/g, "") : sData;
+        sData = $.trim(sData);
+
+        var sValidFirstChars = "0123456789-";
+        var sValidChars = "0123456789.";
+        var Char;
+        var bDecimal = false;
+
+        /* Check for a valid first char (no period and allow negatives) */
+        Char = sData.charAt(0);
+        if (sValidFirstChars.indexOf(Char) == -1) {
+            return null;
+        }
+
+        /* Check all the other characters are valid */
+        for (var i = 1; i < sData.length; i++) {
+            Char = sData.charAt(i);
+            if (sValidChars.indexOf(Char) == -1) {
+                return null;
+            }
+
+            /* Only allowed one decimal place... */
+            if (Char == ".") {
+                if (bDecimal) {
+                    return null;
+                }
+                bDecimal = true;
+            }
+        }
+
+        return 'num-html';
+    });
+
+    $.extend($.fn.dataTableExt.oSort, {
+        "sugar-pre": function (a) {
+            return $.trim(a) == '' ? 0 : Date.create($.trim(a)).getTime();
+        },
+
+        "sugar-asc": function (a, b) {
+            return a - b;
+        },
+
+        "sugar-desc": function (a, b) {
+            return b - a;
+        },
+        "num-html-pre": function (a) {
+            var x = String(a).replace(/<[\s\S]*?>/g, "");
+            return parseFloat(x);
+        },
+
+        "num-html-asc": function (a, b) {
+            return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+        },
+
+        "num-html-desc": function (a, b) {
+            return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+        }
+    });
 
     $.fn.dataTableExt.oStdClasses['sSortAsc'] = 'headerSortDown';
     $.fn.dataTableExt.oStdClasses['sSortDesc'] = 'headerSortUp';
+}
+
+BGB.namespace('tables').init = function () {
+    if ($('table.results').length == 0) {
+        return;
+    }
 
     var navbarHeight = $('.navbar').height();
     $('.content').css('top', navbarHeight + 'px');
 
-    var currentSort = $('th.headerSortUp, th.headerSortDown').map(function(collectionIndex, elem){
+    var currentSort = $('th.headerSortUp, th.headerSortDown').map(function (collectionIndex, elem) {
         return [$(elem).index(), $(elem).hasClass('headerSortUp') ? 'desc' : 'asc'];
     });
 
-    var clientSideSort = $('#results > table.paged').length == 0;
-    var resultsTable = $('#results > table').dataTable({
+    var clientSideSort = $('table.results.paged').length == 0;
+    var resultsTable = $('table.results').dataTable({
         'bPaginate': false,
-        'bSearchable' : false,
         'bFilter': false,
         'bSort': clientSideSort,
-        'aaSorting': [currentSort || [0, 'asc']],
+        'aaSorting': [currentSort.length == 0 ? [0, 'asc'] : currentSort],
         'bInfo': false,
         'bAutoWidth': false,
-        'sScrollY': '',
-        'sScrollX': '',
         'bSortClasses': false
     });
 
-    $('#results table th a').click(function (event) {
+    $('table.results:not(.paged) th a').click(function (event) {
         event.stopPropagation();
     });
 
     new FixedHeader(resultsTable, { offsetTop: navbarHeight });
-    $('.dataTables_wrapper').css('position', '');
+    $('div.dataTables_wrapper').css('position', '');
+    var startingTop = $('table.results').position().top
 
     $('div.fixedHeader').css({
         'position': 'absolute',
-        'top': '0px',
-        'left': '0px'
+        'top': startingTop,
+        'left': 'auto'
     });
 
-    $('#results').append($('div.fixedHeader').detach());
 
-    var updateFixedHeaderPosition = function() {
+
+    $('div.dataTables_wrapper').after($('div.fixedHeader').detach());
+
+    var updateFixedHeaderPosition = function () {
         navbarHeight = $('.navbar').height();
-        var resultsTop = $('#results').offset().top;
+
+        var resultsTop = $('div.dataTables_wrapper').offset().top;
         var newTop;
         if (resultsTop < navbarHeight) {
-            newTop = resultsTop < 0 ? (-resultsTop) + navbarHeight : navbarHeight - resultsTop;
+            newTop = navbarHeight - resultsTop + startingTop;
         } else {
-            newTop = 0;
+            newTop = $('div.dataTables_wrapper').position().top;
         }
+
         $('div.fixedHeader').css({
+            'position': 'absolute',
             'top': newTop + 'px',
-            'left': '0px'
+            'left': 'auto'
         });
     }
 
     $('.content').scroll(updateFixedHeaderPosition);
 
     $(window).resize(function () {
+        if ($(".FixedHeader_Header").length == 0) {
+            return;
+        }
+
         var tableWidth = $("table.results").outerWidth();
 
         $(".FixedHeader_Header").width(tableWidth);
         $(".FixedHeader_Header > table").width(tableWidth);
 
         var columnsWidths = [];
-        $('.dataTables_wrapper th').each(function() {
+        $('.dataTables_wrapper th').each(function () {
             columnsWidths.push($(this).width());
         });
 
@@ -81,4 +160,7 @@ $(document).ready(function() {
         }
         updateFixedHeaderPosition();
     });
-});
+}
+
+$(document).ready(BGB.tables.dataTables.init);
+$(document).ready(BGB.tables.init);
