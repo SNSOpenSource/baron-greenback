7220f2a091fd3dce7bb0da7a4a9a436928619e46
Stuart & Enrico <spmiller@users.noreply.github.com>
2014-01-15 10:39:20 +0000

    Fixing reposition of fixed header after sorting and introducing module pattern.

--- src/com/googlecode/barongreenback/shared/tables.js
+++ src/com/googlecode/barongreenback/shared/tables.js
@@ -1,120 +1,84 @@
-BGB.namespace('tables.dataTables').init = function () {
-    $.fn.dataTableExt.oSort['string-pre'] = function (value) {
-        if (typeof value != "string")  value = value !== null && value.toString ? value.toString() : "";
-        return value;
-    }
+BGB.namespace('tables').init = (function () {
 
-    Date.setLocale("en-UK");
+    var initDataTables = function () {
+        $.fn.dataTableExt.oSort['string-pre'] = function (value) {
+            if (typeof value != "string")  value = value !== null && value.toString ? value.toString() : "";
+            return value;
+        };
 
-    $.fn.dataTableExt.aTypes.unshift(function (sData) {
-        if (sData !== null && !sData.toString().match(/^\d+$/) && Date.create && Date.create(sData).isValid()) {
-            return 'sugar';
-        }
-        return null;
-    });
+        Date.setLocale("en-UK");
 
-    jQuery.fn.dataTableExt.aTypes.unshift(function (sData) {
-        sData = typeof sData.replace == 'function' ?
-            sData.replace(/<[\s\S]*?>/g, "") : sData;
-        sData = $.trim(sData);
-
-        var sValidFirstChars = "0123456789-";
-        var sValidChars = "0123456789.";
-        var Char;
-        var bDecimal = false;
-
-        /* Check for a valid first char (no period and allow negatives) */
-        Char = sData.charAt(0);
-        if (sValidFirstChars.indexOf(Char) == -1) {
+        $.fn.dataTableExt.aTypes.unshift(function (sData) {
+            if (sData !== null && !sData.toString().match(/^\d+$/) && Date.create && Date.create(sData).isValid()) {
+                return 'sugar';
+            }
             return null;
-        }
+        });
 
-        /* Check all the other characters are valid */
-        for (var i = 1; i < sData.length; i++) {
-            Char = sData.charAt(i);
-            if (sValidChars.indexOf(Char) == -1) {
+        jQuery.fn.dataTableExt.aTypes.unshift(function (sData) {
+            sData = typeof sData.replace == 'function' ?
+                sData.replace(/<[\s\S]*?>/g, "") : sData;
+            sData = $.trim(sData);
+
+            var sValidFirstChars = "0123456789-";
+            var sValidChars = "0123456789.";
+            var Char;
+            var bDecimal = false;
+
+            /* Check for a valid first char (no period and allow negatives) */
+            Char = sData.charAt(0);
+            if (sValidFirstChars.indexOf(Char) == -1) {
                 return null;
             }
 
-            /* Only allowed one decimal place... */
-            if (Char == ".") {
-                if (bDecimal) {
+            /* Check all the other characters are valid */
+            for (var i = 1; i < sData.length; i++) {
+                Char = sData.charAt(i);
+                if (sValidChars.indexOf(Char) == -1) {
                     return null;
                 }
-                bDecimal = true;
+
+                /* Only allowed one decimal place... */
+                if (Char == ".") {
+                    if (bDecimal) {
+                        return null;
+                    }
+                    bDecimal = true;
+                }
             }
-        }
 
-        return 'num-html';
-    });
+            return 'num-html';
+        });
 
-    $.extend($.fn.dataTableExt.oSort, {
-        "sugar-pre": function (a) {
-            return $.trim(a) == '' ? 0 : Date.create($.trim(a)).getTime();
-        },
+        $.extend($.fn.dataTableExt.oSort, {
+            "sugar-pre": function (a) {
+                return $.trim(a) == '' ? 0 : Date.create($.trim(a)).getTime();
+            },
 
-        "sugar-asc": function (a, b) {
-            return a - b;
-        },
+            "sugar-asc": function (a, b) {
+                return a - b;
+            },
 
-        "sugar-desc": function (a, b) {
-            return b - a;
-        },
-        "num-html-pre": function (a) {
-            var x = String(a).replace(/<[\s\S]*?>/g, "");
-            return parseFloat(x);
-        },
+            "sugar-desc": function (a, b) {
+                return b - a;
+            },
+            "num-html-pre": function (a) {
+                var x = String(a).replace(/<[\s\S]*?>/g, "");
+                return parseFloat(x);
+            },
 
-        "num-html-asc": function (a, b) {
-            return ((a < b) ? -1 : ((a > b) ? 1 : 0));
-        },
+            "num-html-asc": function (a, b) {
+                return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+            },
 
-        "num-html-desc": function (a, b) {
-            return ((a < b) ? 1 : ((a > b) ? -1 : 0));
-        }
-    });
+            "num-html-desc": function (a, b) {
+                return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+            }
+        });
 
-    $.fn.dataTableExt.oStdClasses['sSortAsc'] = 'headerSortDown';
-    $.fn.dataTableExt.oStdClasses['sSortDesc'] = 'headerSortUp';
-}
-
-BGB.namespace('tables').init = function () {
-    if ($('table.results').length == 0) {
-        return;
-    }
-
-    var currentSort = $('th.headerSortUp, th.headerSortDown').map(function (collectionIndex, elem) {
-        return [$(elem).index(), $(elem).hasClass('headerSortUp') ? 'desc' : 'asc'];
-    });
-
-    var clientSideSort = $('table.results.paged').length == 0;
-    var resultsTable = $('table.results').dataTable({
-        'bPaginate': false,
-        'bFilter': false,
-        'bSort': clientSideSort,
-        'aaSorting': [currentSort.length == 0 ? [0, 'asc'] : currentSort],
-        'bInfo': false,
-        'bAutoWidth': false,
-        'bSortClasses': false
-    });
-
-    $('table.results:not(.paged) th a').click(function (event) {
-        event.stopPropagation();
-    });
-
-    new FixedHeader(resultsTable, { offsetTop: $('.navbar').height()});
-    $('div.dataTables_wrapper').css('position', '');
-    var startingTop = $('table.results').position().top;
-
-    $('div.fixedHeader').css({
-        'position': 'absolute',
-        'top': startingTop,
-        'left': 'auto'
-    });
-
-
-
-    $('div.dataTables_wrapper').after($('div.fixedHeader').detach());
+        $.fn.dataTableExt.oStdClasses['sSortAsc'] = 'headerSortDown';
+        $.fn.dataTableExt.oStdClasses['sSortDesc'] = 'headerSortUp';
+    };
 
     var updateFixedHeaderPosition = function () {
         var navbarHeight = $('.navbar').height();
@@ -135,30 +99,87 @@ BGB.namespace('tables').init = function () {
         });
     };
 
-    $(window).scroll(updateFixedHeaderPosition);
+    var registerAfterDrawCallback = function (table, funct) {
+        var settings = table.fnSettings();
+        settings.aoDrawCallback.unshift({
+            'fn': funct,
+            'sName': 'user'
+        });
+    };
 
-    $(window).resize(function () {
-        if ($(".FixedHeader_Header").length == 0) {
+    var initFixedHeader = function () {
+        if ($('table.results').length == 0) {
             return;
         }
 
-        var tableWidth = $("table.results").outerWidth();
-
-        $(".FixedHeader_Header").width(tableWidth);
-        $(".FixedHeader_Header > table").width(tableWidth);
-
-        var columnsWidths = [];
-        $('.dataTables_wrapper th').each(function () {
-            columnsWidths.push($(this).width());
+        var currentSort = $('th.headerSortUp, th.headerSortDown').map(function (collectionIndex, elem) {
+            return [$(elem).index(), $(elem).hasClass('headerSortUp') ? 'desc' : 'asc'];
         });
 
-        var fixedHeaderCells = $('.FixedHeader_Header th')
-        for (var count = 0; count < columnsWidths.length; count++) {
-            $(fixedHeaderCells[count]).css('width', columnsWidths[count] + 'px');
-        }
-        updateFixedHeaderPosition();
-    });
-}
+        var clientSideSort = $('table.results.paged').length == 0;
+        var resultsTable = $('table.results').dataTable({
+            'bPaginate': false,
+            'bFilter': false,
+            'bSort': clientSideSort,
+            'aaSorting': [currentSort.length == 0 ? [0, 'asc'] : currentSort],
+            'bInfo': false,
+            'bAutoWidth': false,
+            'bSortClasses': false
+        });
 
-$(document).ready(BGB.tables.dataTables.init);
+        $('table.results:not(.paged) th a').click(function (event) {
+            event.stopPropagation();
+        });
+
+        new FixedHeader(resultsTable, { offsetTop: $('.navbar').height()});
+
+        /* This has to be done here because the FixedHeader plugin introduces some logic to reposition the header that we want to override. */
+        registerAfterDrawCallback(resultsTable, updateFixedHeaderPosition);
+
+
+        $('div.dataTables_wrapper').css('position', '');
+        var startingTop = $('table.results').position().top;
+
+        $('div.fixedHeader').css({
+            'position': 'absolute',
+            'top': startingTop,
+            'left': 'auto'
+        });
+
+        $('div.dataTables_wrapper').after($('div.fixedHeader').detach());
+    };
+
+    var registerWindowEvents = function () {
+        $(window).scroll(updateFixedHeaderPosition);
+
+        $(window).resize(function () {
+            if ($(".FixedHeader_Header").length == 0) {
+                return;
+            }
+
+            var tableWidth = $("table.results").outerWidth();
+
+            $(".FixedHeader_Header").width(tableWidth);
+            $(".FixedHeader_Header > table").width(tableWidth);
+
+            var columnsWidths = [];
+            $('.dataTables_wrapper th').each(function () {
+                columnsWidths.push($(this).width());
+            });
+
+            var fixedHeaderCells = $('.FixedHeader_Header th')
+            for (var count = 0; count < columnsWidths.length; count++) {
+                $(fixedHeaderCells[count]).css('width', columnsWidths[count] + 'px');
+            }
+            updateFixedHeaderPosition();
+        });
+    };
+
+    return function () {
+        initDataTables();
+        initFixedHeader();
+        registerWindowEvents();
+    };
+})();
+
 $(document).ready(BGB.tables.init);
