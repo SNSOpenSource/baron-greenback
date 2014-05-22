2f9deff0d47f8d13542a630dcad4fc687833df33
Jorge Pastor <jorpaspr@users.noreply.github.com>
2014-05-22 16:03:41 +0100

    Changing job history item lifespan default value from 30 days to 24hr and making it configurable via a property.

--- src/com/googlecode/barongreenback/jobshistory/JobHistoryItemLifespanInHours.java
+++ src/com/googlecode/barongreenback/jobshistory/JobHistoryItemLifespanInHours.java
@@ -2,14 +2,17 @@ package com.googlecode.barongreenback.jobshistory;
 
 import com.googlecode.totallylazy.Value;
 
+import java.util.Properties;
+
 public class JobHistoryItemLifespanInHours implements Value<Integer> {
 
-    public final static Integer DEFAULT = 720; // 30 days
+    public final static Integer DEFAULT = 24;
+    public final static String PROPERTY_NAME = "baron-greenback.job.history.item.lifespan.in.hours";
 
     private final Integer value;
 
-    public JobHistoryItemLifespanInHours(){
-        this(DEFAULT);
+    public JobHistoryItemLifespanInHours(Properties properties) {
+        this(Integer.valueOf(properties.getProperty(PROPERTY_NAME, String.valueOf(DEFAULT))));
     }
 
     public JobHistoryItemLifespanInHours(Integer value) {
