28cf5f0c2d3176bb1445a6d3c598be45a909f4ee
Stuart Miller <spmiller@users.noreply.github.com>
2015-04-17 18:45:20 +0100

    Fix uploading path

--- build/bintrayUpload.sh
+++ build/bintrayUpload.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 shopt -s nullglob
 for f in artifacts/baron-greenback-$BUILD_NUMBER*
 do
-	curl -T $f -usns-deployer:$BINTRAY_KEY -H "X-Bintray-Package:baron-greenback" -H "X-Bintray-Version:$BUILD_NUMBER" https://api.bintray.com/content/sns/baron-greenback/sky/sns/baron-greenback/$(basename $f)
+	curl -T $f -usns-deployer:$BINTRAY_KEY -H "X-Bintray-Package:baron-greenback" -H "X-Bintray-Version:$BUILD_NUMBER" https://api.bintray.com/content/sns/baron-greenback/sky/sns/baron-greenback/baron-greenback/$BUILD_NUMBER/$(basename $f)
 done
