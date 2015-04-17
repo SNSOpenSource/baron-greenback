729f5d0d9e446c850ef58f2bee6734199f46ac6c
Stuart Miller <spmiller@users.noreply.github.com>
2015-04-17 22:05:45 +0100

    Correct path to artifacts

--- build/bintrayUpload.sh
+++ build/bintrayUpload.sh
@@ -2,7 +2,7 @@
 echo "Deploying to bintray"
 
 shopt -s nullglob
-for f in artifacts/baron-greenback-$BUILD_NUMBER*
+for f in ./build/artifacts/baron-greenback-$BUILD_NUMBER*
 do
 	curl -T $f -usns-deployer:$BINTRAY_KEY -H "X-Bintray-Package:baron-greenback" -H "X-Bintray-Version:$BUILD_NUMBER" https://api.bintray.com/content/sns/baron-greenback/sky/sns/baron-greenback/baron-greenback/$BUILD_NUMBER/$(basename $f)
 done
