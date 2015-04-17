121a362969cdafbb554f9f0d6cbbd3e5570a1936
Stuart Miller <spmiller@users.noreply.github.com>
2015-04-17 22:22:21 +0100

    Add more debugging

--- build/bintrayUpload.sh
+++ build/bintrayUpload.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "Deploying to bintray"
+echo "Deploying '$BUILD_NUMBER' to bintray"
 
 shopt -s nullglob
 for f in ./build/artifacts/baron-greenback-$BUILD_NUMBER*
 do
+	echo $(basename $f)
 	curl -T $f -usns-deployer:$BINTRAY_KEY -H "X-Bintray-Package:baron-greenback" -H "X-Bintray-Version:$BUILD_NUMBER" https://api.bintray.com/content/sns/baron-greenback/sky/sns/baron-greenback/baron-greenback/$BUILD_NUMBER/$(basename $f)
+	echo
 done
