6a6158335d08eda7914e4a4fd2cc755d3a7d2055
Stuart <spmiller@users.noreply.github.com>
2015-05-18 17:33:55 +0100

    Don't try to upload artifacts if the bintray api key has not been set

--- build/bintrayUpload.sh
+++ build/bintrayUpload.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-echo "Deploying '$BUILD_NUMBER' to bintray"
+if [ -z ${BINTRAY_KEY+x} ];
+then
+	echo "Not uploading artifacts to bintray as BINTRAY_KEY is not set"
+else 
+	echo "Deploying '$BUILD_NUMBER' to bintray"
 
-shopt -s nullglob
-for f in ./build/artifacts/baron-greenback-$BUILD_NUMBER*
-do
-	echo $(basename $f)
-	curl -T $f -usns-deployer:$BINTRAY_KEY -H "X-Bintray-Package:baron-greenback" -H "X-Bintray-Version:$BUILD_NUMBER" -H "X-Bintray-Publish: 1" https://api.bintray.com/content/sns/baron-greenback/com/sky/sns/baron-greenback/$BUILD_NUMBER/$(basename $f)
-	echo
-done
+	shopt -s nullglob
+	for f in ./build/artifacts/baron-greenback-$BUILD_NUMBER*
+	do
+		echo $(basename $f)
+		curl -T $f -usns-deployer:$BINTRAY_KEY -H "X-Bintray-Package:baron-greenback" -H "X-Bintray-Version:$BUILD_NUMBER" -H "X-Bintray-Publish: 1" https://api.bintray.com/content/sns/baron-greenback/com/sky/sns/baron-greenback/$BUILD_NUMBER/$(basename $f)
+		echo
+	done
+
+fi
+
+
